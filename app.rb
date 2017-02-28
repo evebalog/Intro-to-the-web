04a3f67b58e1a68fdc54e0bc5d5c752e899e1ec3
Eva Balog <evebalog@netscape.net>
2017-02-28 11:55:12 +0000

    Created input tags

--- app.rb
+++ app.rb
@@ -17,7 +17,13 @@ get '/eva' do
   "Misses cycling a lot"
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
